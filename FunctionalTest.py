d4d309d62800739449da1294866095a6a199809e
carla fhae <carlafhae.torres@gsfe.tupcavite.edu.ph>
2021-04-07 04:46:38 +0800

    This is my First Functional Test Passed

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -6,13 +6,13 @@ class PageTest(unittest.TestCase):
    def setUp(self):
       self.browser = webdriver.Firefox()
    
-   def tearDown(self):
-      self.browser.quit()
+   #def tearDown(self):
+   #   self.browser.quit()
       
    def test_browser_title(self):
-      self.browser.get('http://localhost:8000')
-      self.assertIn('Carla List', self.browser.title)
-      self.fail('Finish the test NOW!')
+      self.browser.get('http://localhost:8100')
+      self.assertIn('Recipe List', self.browser.title)
+      #self.fail('Finish the test!')
       
       
 if __name__ == '__main__':
--- RecipeList/views.py
+++ RecipeList/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
-def MainPage():
-	pass
+def MainPage(request):
+	return HttpResponse('<html><title>Recipe List</title><h1 style="color:blue; font-size=35px">Three Main Course Recipe List</h1><input type="text" id="newEntry" name="Entry1" placeholder="What recipe do you want to request&#063">&nbsp<input type="submit" value="Confirm"></html')
+
