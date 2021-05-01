16c510c69de99612f854c74c94e5833c5ad73238
carla fhae <carlafhae.torres@gsfe.tupcavite.edu.ph>
2021-05-02 03:17:56 +0800

    Model.py adding

--- LoanApp/models.py
+++ LoanApp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Recruit(models.Model):
+	pass
 class Item(models.Model):
+	RecId = models.ForeignKey(Recruit, default=None, on_delete=models.CASCADE)
 	text = models.TextField(default="")
+
+
 	#addr = models.IntField(default="")
 	#addrSt = models.CharField(default="")
